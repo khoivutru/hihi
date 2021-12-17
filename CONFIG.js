b026c54befd7fa657ab2a155464ea73e7e0726ca
khoivutru <82798521+khoivutru@users.noreply.github.com>
2021-12-17 21:38:24 +0700

    Add files via upload

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Có Thích Anh Không",
+  descriptionHeader:
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
+  buttonYes: "Đồng Ý",
+  buttonNo: "Không! Còn lâu nhé.",
+  titleModar: "Cảm ơn em vì đồng ý",
+  descriptionModar:
+    'Anh  biết là em sẽ đồng ý làm người yêu anh mà ^^ </br> Cám ơn em đã đồng ý để cho tớ được che chở và bảo vệ em.Đối với tớ, cậu mãi là cô bé :)) </br> "Yêu em như việc thở. </br> làm sao anh có thể ngừng"',
+};
