2c27e6d1421fda66e4b311caca0c6d47560bd8a5
khoivutru <khoivutru@gmail.com>
2021-12-17 21:28:06 +0700

    first commit

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Có Thích Anh Không",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
+    "Nếu cậu thoát ra và không trả lời thì có nghĩa là em đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Cảm ơn em vì đồng ý",
